151d4dc047be407a66fa3cc4bc0a7b2497f1c623
DarkSylar117 <t.anto.j@gmail.com>
2018-06-08 00:32:42 -0500

    Add: Arduino sketch

---
+++ arduino/Osciloscopio/Osciloscopio.ino
@@ -0,0 +1,28 @@
+#include <SerialCommand.h>      // Steven Cogswell ArduinoSerialCommand library from http://GitHub.com
+#include <SoftwareSerial.h>
+
+#define ANALOG_READ 0
+
+
+SerialCommand     serialCommand;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  analogReference(EXTERNAL);
+  serialCommand.addCommand("r", ReadVolts );
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  serialCommand.readSerial();
+}
+
+
+void ReadVolts() {
+  int value = 0;
+  value = analogRead(ANALOG_READ);
+  Serial.write(value / 4);
+  Serial.println(value / 4);
+}
+
---
+++ arduino/OsciloscopioOriginal/OsciloscopioOriginal.ino
@@ -0,0 +1,77 @@
+
+void setup()
+{
+  Serial.begin(115200);
+  analogReference(EXTERNAL);
+
+  Inicializa();
+}
+
+void loop()
+{
+  Get_Instruccion();
+}
+
+
+
+int Get_Instruccion()
+{
+  if(Serial.available())
+  {
+      switch(Serial.read())
+      {
+         case 'r'://Leer entrada analogica y mandar dato
+           Analog_Read();
+         break; 
+         
+         case 's': //En espera a nueva inicializacion
+           Salir();
+         break;
+      }
+  }
+}
+
+void Salir()
+{
+  Inicializa(); //Loop en inicializa esperando nueva inicializacion
+}
+
+void Analog_Read()
+{
+    int resultado = 0;
+    resultado = analogRead(0); //Leemos el pin analogico 0 en 10bits
+    Serial.write(resultado / 4); //Mandamos el resultado en 8bits por Serial
+}
+
+
+
+//Inicializamos la comunicacion serial
+//Debe mandar una secuencia de caracteres tal que cumplan con el criterio "square"
+void Inicializa()
+{
+  int caracter[6];
+  int i = 0;
+  caracter[0] = 's';
+  caracter[1] = 'q';
+  caracter[2] = 'u';
+  caracter[3] = 'a';
+  caracter[4] = 'r';
+  caracter[5] = 'e';
+  
+  while(1)
+  {//Mientras no se inicialize no sale del ciclo
+     if(Serial.available())
+    {
+        if(Serial.read() == caracter[i])
+        {
+          if(i == 5)
+          {//Si es la ultima iteracion
+            Serial.write('a');//escribimos caracter de confirmacion
+            break; //Salimos del while
+          }
+          i++;
+        }
+      } 
+  }
+  //Termina While
+}
